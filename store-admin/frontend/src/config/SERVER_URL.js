07299ee08e6352f8b33c9a4c9d2b90f5f3cdd5f6
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-20 12:42:20 +0530

    new commit

--- store-admin/frontend/src/config/SERVER_URL.js
+++ store-admin/frontend/src/config/SERVER_URL.js
@@ -1,7 +1,7 @@
 // const SERVER_URL = "http://localhost:3001/api"
 // const SERVER_URL = "https://award-store.onrender.com/api"
 
-const SERVER_URL = "https://avard-store.plusitpark.com/api"
+const SERVER_URL = "https://medal-avard-store.plusitpark.com/api"
 
 
 export default SERVER_URL;
